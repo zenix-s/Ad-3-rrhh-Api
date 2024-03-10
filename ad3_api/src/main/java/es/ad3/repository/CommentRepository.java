c9d652d0975e7bd77fa00a5ccda7018a9a10f376
Sergio Fernández <80365583+zenix-s@users.noreply.github.com>
2024-03-10 18:35:43 +0000

    Remove unused files and code

--- ad3_api/src/main/java/es/ad3/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package es.ad3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.ad3.model.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer>{
-	
-}
--- ad3_api/src/main/java/es/ad3/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package es.ad3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.ad3.model.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
